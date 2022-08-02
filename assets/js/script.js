3d90fdb339e82eb261a0e61770483cba5dfe82cf
Mac Schmitz <mac.schmitz@att.net>
2022-08-01 23:37:08 -0400

    created function to populate the schedule with 1 hour time blocks from 7AM until 10PM

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var now = moment().local();
+var scheduleItem = [];
+var startTime = 7;
+var endTime = 22;
+
+function init() {
+    createSchedule();
+  }
+  
+  /* Writes the html for the calendar to display on the page */
+  function createSchedule() {
+    for (var i = startTime; i <= endTime; i++) {
+      var hour = moment(i, "H").format("hA");
+  
+      $(".container").append(`<div class="row">
+          <div class="col-1 hour" id="hour${i}">${hour}</div>
+          <input class="col-9 textArea event" id="time${i}"></input>
+          <i class="fas fa-save col-1 saveBtn"></i>
+          <i class="fa fa-trash col-1 deleteBtn"></i>
+        </div>`);
+    }
+  }
+  
+  init();
